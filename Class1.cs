6d53eda0400e3d29ade8ee965ed3c1e6820dc2df
SAPR <SAPR@PC-NXT>
2025-09-10 12:21:26 +0700

    123

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1
+{
+    internal class Class1
+    {
+    }
+}
+public class Point2D
+{
+    //Атрибуты класса
+    private int X;
+    private int Y;
+    //Конструктор класса
+    public Point2D(int x, int y)
+    {
+        //this используется для однозначного указания на атрибуты класса так как переменные имеют одинаковые имена
+    this.X = x;
+        this.Y = y;
+    }
+
+    //Методы для получения координат
+    public int getX()
+    {
+        return X;
+    }
+    public int getY()
+    {
+        return Y;
+    }
+    //Методы для изменения координат
+    public void addX(int x)
+    {
+        X += x;
+    }
+    public void addY(int y)
+    {
+        Y += y;
+    }
+
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,11 +14,98 @@ namespace Laba_1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
+    
+    
+    
     public partial class MainWindow : Window
     {
+
+        Triangle tr;
+        Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
+            //Создание треугольника со случайными координатами
+            Point2D p1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0,(int)Scene.Height));
+            Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0,(int)Scene.Height));
+            Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0,(int)Scene.Height));
+            tr = new Triangle(p1, p2, p3);
         }
+        public void DrawTriangle(Triangle tr)
+        {
+            //Отрисовка треугольника с помощью функции отрисовки линии
+            DrawLine(tr.getP1(), tr.getP2());
+            DrawLine(tr.getP2(), tr.getP3());
+            DrawLine(tr.getP3(), tr.getP1());
+        }
+        public void ClearScene()
+        {
+            //Очистка Canvas от всех объектов
+            Scene.Children.Clear();
+        }
+
+
+        //функция в основном теле программы
+        public void DrawLine(Point2D p1, Point2D p2)
+        {
+            //Создание новой линии
+            Line line = new Line();
+            //Цвет и толщина линии
+            line.Stroke = Brushes.Red;
+            line.StrokeThickness = 3;
+            //Установка координат линии из координат точек Point2D
+            line.X1 = p1.getX();
+            line.Y1 = p1.getY();
+            line.X2 = p2.getX();
+            line.Y2 = p2.getY();
+            //Добавление линии в Canvas
+            Scene.Children.Add(line);
+        }
+
+
     }
+
+
+    public class Point2D
+    {
+        //Атрибуты класса
+        private int X;
+        private int Y;
+        //Конструктор класса
+        public Point2D(int x, int y)
+        {
+            //this используется для однозначного указания на атрибуты класса так как переменные имеют одинаковые имена
+            this.X = x;
+            this.Y = y;
+        }
+
+        //Методы для получения координат
+        public int getX()
+        {
+            return X;
+        }
+        public int getY()
+        {
+            return Y;
+        }
+        //Методы для изменения координат
+        public void addX(int x)
+        {
+            X += x;
+        }
+        public void addY(int y)
+        {
+            Y += y;
+        }
+
+
+
+
+
+    }
+
+
+
+
 }
